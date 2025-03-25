8852087668c0b79c33b0356f854badfc979884f0
Sean <seangores@gmail.com>
2025-03-25 16:28:11 -0600

    feat: add comprehensive MPV configuration files and shaders for enhanced media playback experience

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -14,6 +14,8 @@
 
 	# Copy all your MPV config files
 	xdg.configFile = {
+		"mpv/mpv.conf".source = ../dotfiles/mpv/mpv.conf;
+		"mpv/input.conf".source = ../dotfiles/mpv/input.conf;
 		"mpv/configs/mpv_osd.conf".source = ../dotfiles/mpv/configs/mpv_osd.conf;
 		"mpv/configs/mpv_shaders.conf".source = ../dotfiles/mpv/configs/mpv_shaders.conf;
 		"mpv/configs/mpv_amd_or_intel_gpu.conf".source = ../dotfiles/mpv/configs/mpv_amd_or_intel_gpu.conf;
@@ -21,5 +23,9 @@
 		"mpv/configs/mpv_nvidia_gpu.conf".source = ../dotfiles/mpv/configs/mpv_nvidia_gpu.conf;
 		"mpv/configs/mpv_subtitles.conf".source = ../dotfiles/mpv/configs/mpv_subtitles.conf;
 		"mpv/configs/mpv_terminal.conf".source = ../dotfiles/mpv/configs/mpv_terminal.conf;
+		"mpv/fonts".source = ../dotfiles/mpv/fonts;
+		"mpv/scripts".source = ../dotfiles/mpv/scripts;
+		"mpv/shaders".source = ../dotfiles/mpv/shaders;
+		"mpv/script-opts".source = ../dotfiles/mpv/script-opts;
 	};
 }

bdf933a354ef5aea7a9f6055c933189081374a25
Sean <seangores@gmail.com>
2025-03-25 15:57:51 -0600

    refactor: standardize formatting in Fish shell configuration files for improved readability and consistency

--- dotfiles/fish/functions/_nvm_version_activate.fish
+++ dotfiles/fish/functions/_nvm_version_activate.fish
@@ -1,4 +1,4 @@
 function _nvm_version_activate --argument-names ver
-    set --global --export nvm_current_version $ver
-    set --prepend PATH $nvm_data/$ver/bin
+	set --global --export nvm_current_version $ver
+	set --prepend PATH $nvm_data/$ver/bin
 end
